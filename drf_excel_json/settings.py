05f254e2355148b6c31c2bac8c01a2afcc3a1336
amit <yadavamit.ay@gmail.com>
2021-06-19 18:48:01 +0530

    added static files settings and directory

--- drf_excel_json/settings.py
+++ drf_excel_json/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, '_static'),
+)
